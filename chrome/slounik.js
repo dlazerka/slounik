f332ff3a34dfd6419e5905a7af37367dd980e7b6
Dzmitry Lazerka <dlazerka@gmail.com>
2015-11-02 21:42:33 -0800

    Updated Chrome extension for the current server.
    Updated GAE version.

--- chrome/slounik.js
+++ chrome/slounik.js
@@ -1,108 +1,135 @@
-/** Copyright Dzmitry Lazerka <me@dlazerka.name>
- *  http://slounik.dlazerka.name/
- *  Licensed on GPL terms: http://www.gnu.org/licenses/gpl.html
+/*
+ *    Copyright 2015 Dzmitry Lazerka
+ *
+ *    This file is part of Slounik.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-$(function() {
-	var SEARCH_URL = location.href + 'rpc/search';
-	if (window.location.protocol == 'chrome-extension:') {
-		var SEARCH_URL = 'http://slounik.dlazerka.name/rpc/search';
-	}
-	var inputEl = $('.input');
-	var rightEl = $('.right .words');
-	var leftEl = $('.left .words');
-	var arrowsEl = $('.arrows');
 
-	function toLines(data) {
-		var lines = [];
-		var query = $('query', data).text();
-		$('entry', data).each(function(index, entry) {
-			var line = {};
-			lines.push(line);
-			$('lexeme', entry).each(function(index, lexeme) {
-				var word = $(lexeme).text();
-				if (word.search(query) == 0) {
-					word = word.substring(query.length);
-					var span = '<span class="query">' + query + '</span>'
-					word = span + word;
-					line['arrow'] = line['arrow'] ? '&harr;' :
-						(index ? '&larr;' : '&rarr;');
-				}
-				var lang = $(lexeme).attr('lang');
-				line[lang] = word;
-			});
-		});
-		return lines;
-	};
+'use strict';
 
-	/** Don't compare arrows, only words. */
-	function linesComparator(line1, line2) {
-		/** First by length, then naively. */
-		function wordsComparator(word1, word2) {
-			if (word1.length != word2.length) {
-				return word1.length - word2.length;
+angular.module('name.dlazerka.slounik', [])
+	.constant('REST_API_URL', 'http://slounik.dlazerka.name/rest')
+	.directive('preselect', function($timeout) {
+		return {
+			link: function($scope, element, attrs) {
+				// Have to do $timeout, because DOM is not yet synced with $scope.
+				$timeout(function() {
+					element.select();
+				});
 			}
-			if (word1 == word2) return 0;
-			return 1 - 2*Number(word1 < word2);
-		};
-		var word1 = line1['arrow'] == '&larr;' ? line1['ru'] : line1['be'];
-		var word2 = line2['arrow'] == '&larr;' ? line2['ru'] : line2['be'];
-		var word1_ = line1['arrow'] == '&rarr;' ? line2['ru'] : line2['be'];
-		var word2_ = line2['arrow'] == '&rarr;' ? line2['ru'] : line2['be'];
-		if (word1 != word2) {
-			return wordsComparator(word1, word2);
-		} else if (word1 != word2_) {
-			return wordsComparator(word1, word2_);
-		} else if (word2 != word1_) {
-			return wordsComparator(word1_, word2);
-		} else {
-			return 0;
 		}
-	};
+	})
+	.controller('SlounikController', function($scope, $http, REST_API_URL) {
+		$scope.input = localStorage.getItem('input') || 'слоўнік';
+		$scope.results = [];
 
-	function found(data, textStatus, jqXHR) {
-		var lines = toLines(data);
-		if (!lines.length) {
-			inputEl.addClass('noResults');
-			return;
+		// $scope.found to show/hide "not found" message.
+		$scope.searching = false;
+		$scope.found = true;
+
+		$scope.$watch('input', function() {
+			localStorage.setItem('input', $scope.input);
+			if (!$scope.input) return;
+			$scope.searching = true;
+
+			$http.get(REST_API_URL + '/entry/search/be/ru/' + encodeURI($scope.input))
+				// binding requested value so we can handle race conditions.
+				.then(resultsArrived.bind(this, $scope.input));
+		});
+
+		function resultsArrived(requestedInput, response) {
+			if ($scope.input != requestedInput) {
+				// User already typed more characters, so abandoning the result.
+				return;
+			}
+			$scope.searching = false;
+
+			/** Divides response onto `match` and `rest`. */
+			function Translation(lemma, dict) {
+				this.lemma = lemma;
+
+				if (lemma.indexOf($scope.input) == 0) {
+					this.match = $scope.input;
+					this.rest = lemma.substr($scope.input.length);
+				} else {
+					this.match = '';
+					this.rest = lemma;
+				}
+				this.dicts = [dict];
+			}
+
+			var rows = [];
+			var rowByLemma = {
+				ru: {},
+				be: {}
+			};
+			angular.forEach(response.data, function(res) {
+				var lemma = new Translation(res.lemma, res.dict);
+				var translations = res.translations.map(function(translation) {
+					return new Translation(translation, res.dict);
+				});
+
+				var row = {
+					ru: {
+						translations: res.from == 'ru' ? [lemma] : translations,
+						dict: null
+					},
+					be: {
+						translations: res.from == 'be' ? [lemma] : translations,
+						dict: null
+					},
+					arrow: res.from == 'ru' ? '←' : '→'
+				};
+
+				var existingRow = rowByLemma[res.from][res.lemma];
+				if (existingRow) {
+					// Merge with existingRow
+					angular.forEach(['ru', 'be'], function(lang) {
+						var newTranslations = [];
+
+						angular.forEach(existingRow[lang].translations, function(translation) {
+							// Was the translation already seen?
+							var newTranslation = row[lang].translations.filter(function(newTranslation) {
+								return newTranslation.lemma == translation.lemma;
+							})[0];
+
+							if (newTranslation) {
+								// Then don't add the translation, only add it's dict.
+								translation.dicts = translation.dicts.concat(newTranslation.dicts);
+							} else {
+								newTranslations = row[lang].translations;
+							}
+						});
+						existingRow[lang].translations = existingRow[lang].translations.concat(newTranslations);
+					});
+				} else {
+					rows.push(row);
+					rowByLemma[res.from][res.lemma] = row;
+				}
+			});
+
+			$scope.rows = rows;
+			$scope.found = rows.length > 0;
+
+			$scope.mouseOver = function(translation, row) {
+				row.dict = translation.dicts.join(', ');
+			};
+
+			$scope.mouseOut = function(row) {
+				row.dict = null;
+			};
 		}
-		inputEl.removeClass('noResults');
-		inputEl.select();
-
-		lines.sort(linesComparator);
-
-		var beWords = lines.map(function(line) {
-			return line['be'];
-		});
-		var ruWords = lines.map(function(line) {
-			return line['ru'];
-		});
-		var arrows = lines.map(function(line) {
-			return line['arrow'];
-		});
-
-//		var lastLine = lines[lines.length - 1];
-//		leftEl.css('min-width', lastLine['be'].length + 'ex');
-//		rightEl.css('min-width', lastLine['ru'].length + 'ex');
-
-		leftEl.html(beWords.join('<br/>'));
-		rightEl.html(ruWords.join('<br/>'));
-		arrowsEl.html('<br/>' + arrows.join('<br/>'));
-		rightEl.show();
-		arrowsEl.show();
-		leftEl.show();
-	};
-
-	inputEl.bind('keyup', function(event) {
-		inputEl.removeClass('noResults');
-		if (event.keyCode != 13) return;
-		$.get(SEARCH_URL, {'q': inputEl.val()}, found, 'xml');
-		rightEl.hide();
-		arrowsEl.hide('');
-		leftEl.hide('');
-	});
-	inputEl.bind('touchend', function(event) {
-		inputEl.select();
-	});
-	inputEl.select();
-	$.get(SEARCH_URL, {'q': inputEl.val()}, found, 'xml');
-});
+	})
+;
--- gae/src/main/webapp/slounik.js
+++ gae/src/main/webapp/slounik.js
@@ -1,5 +1,25 @@
+/*
+ *    Copyright 2015 Dzmitry Lazerka
+ *
+ *    This file is part of Slounik.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 'use strict';
-angular.module('me.lazerka.slounik', [])
+
+angular.module('name.dlazerka.slounik', [])
 	.directive('preselect', function($timeout) {
 		return {
 			link: function($scope, element, attrs) {
@@ -10,24 +30,6 @@ angular.module('me.lazerka.slounik', [])
 			}
 		}
 	})
-/*
-	.factory('DictService', function() {
-		var dicts = {};
-
-		$http.get('/rest/dict/be/ru')
-			.then(function(response) {
-				angular.forEach(response.data, function(res) {
-					dicts[res.id] = dict;
-				});
-			});
-
-		return {
-			getDict : function(dictId) {
-				return dicts[dictId];
-			}
-		}
-	})
-*/
 	.controller('SlounikController', function($scope, $http) {
 		$scope.input = localStorage.getItem('input') || 'слоўнік';
 		$scope.results = [];
