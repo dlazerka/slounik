f2e6d7af369808130fd8796b235e90043d22557d
Dzmitry Lazerka <dlazerka@gmail.com>
2015-11-06 14:16:13 -0800

    Deleted old sources.

--- gae/src/main/scala/name/dlazerka/slounik/QueryException.scala
+++
@@ -1,5 +0,0 @@
-package name.dlazerka.slounik
-
-class QueryException(message: String) extends Exception(message) {
-	def this() = this(null)
-}
--- gae/src/main/scala/name/dlazerka/slounik/admin/Datastores.scala
+++
@@ -1,7 +0,0 @@
-package name.dlazerka.slounik.admin
-import com.google.appengine.api.datastore.DatastoreServiceFactory
-
-trait Datastores {
-	val datastore = DatastoreServiceFactory.getDatastoreService()
-	val asyncDatastore = DatastoreServiceFactory.getAsyncDatastoreService()
-}
