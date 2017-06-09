de2babc12a4565aa4d9eca395c4600dcc435852c
Dzmitry Lazerka <dlazerka@gmail.com>
2017-06-09 00:42:48 -0700

    Updated somewhat.

--- tools/src/main/scala/name/dlazerka/slounik/tools/build/PreDeploy.scala
+++ tools/src/main/scala/name/dlazerka/slounik/tools/build/PreDeploy.scala
@@ -27,83 +27,84 @@ import sun.misc.IOUtils
 import scala.io.Source
 
 /**
- * Processes all .html files:
- * Searches for strings like
- * {{{
- * &lt;script src="lib/jquery.js" predeploy="//googleapis.cdn.com/jquery.min.js"&gt;
- * }}}
- * and replaces `src` with `predeploy`.
- *
- * @author Dzmitry Lazerka
- */
+  * Processes all .html files:
+  * Searches for strings like
+  * {{{
+  * &lt;script src="lib/jquery.js" predeploy="//googleapis.cdn.com/jquery.min.js"&gt;
+  * }}}
+  * and replaces `src` with `predeploy`.
+  *
+  * @author Dzmitry Lazerka
+  */
 object PreDeploy {
-	val workingDir = System.getProperty("user.dir")
+  private val workingDir = System.getProperty("user.dir")
 
-	def main(args: Array[String]) {
-		val workingFile = new File(workingDir)
-		println("Traversing " + workingFile.getAbsolutePath)
-		traverse(workingFile)
-	}
+  def main(args: Array[String]) {
+    val workingFile = new File(workingDir)
+    println("Traversing " + workingFile.getAbsolutePath)
+    traverse(workingFile)
+  }
 
-	def traverse(dir: File): Unit = {
-		val nodes = dir.listFiles()
+  def traverse(dir: File): Unit = {
+    val nodes = dir.listFiles()
 
-		nodes.filter(node => node.isFile && node.getName.endsWith(".html"))
-			.map(processHtml)
+    nodes.filter(node => node.isFile && node.getName.endsWith(".html"))
+      .foreach(processHtml)
 
-		nodes.filter(_.isDirectory)
-			.map(traverse)
-	}
+    nodes.filter(_.isDirectory)
+      .foreach(traverse)
+  }
 
-	def processHtml(htmlFile: File): Unit = {
-		assume(htmlFile.length() < Integer.MAX_VALUE)
-		assume(htmlFile.canRead)
-		val source = Source.fromFile(htmlFile, "UTF-8", htmlFile.length().toInt)
-		val content = source.mkString
-		source.close()
+  def processHtml(htmlFile: File): Unit = {
+    assume(htmlFile.length() < Integer.MAX_VALUE)
+    assume(htmlFile.canRead)
+    val source = Source.fromFile(htmlFile, "UTF-8", htmlFile.length().toInt)
+    val content = source.mkString
+    source.close()
 
-		var newContent = """(src=)"([^"]+)"\s+predeploy="([^"]+)""""
-				.r
-				.replaceAllIn(content, """$1"$3"""")
+    var newContent =
+      """(src=)"([^"]+)"\s+predeploy="([^"]+)""""
+        .r
+        .replaceAllIn(content, """$1"$3"""")
 
-		newContent = processEtags(newContent, htmlFile)
+    newContent = processEtags(newContent, htmlFile)
 
-		if (newContent.equals(content)) {
-			println("Nothing replaced in " + htmlFile.getAbsolutePath)
-			return
-		}
+    if (newContent.equals(content)) {
+      println("Nothing replaced in " + htmlFile.getAbsolutePath)
+      return
+    }
 
-		val writer = new FileWriter(htmlFile)
-		try {
-			writer.write(newContent)
-		} finally {
-			writer.close()
-		}
+    val writer = new FileWriter(htmlFile)
+    try {
+      writer.write(newContent)
+    } finally {
+      writer.close()
+    }
 
-		println("Processed " + htmlFile.getAbsolutePath)
-	}
+    println("Processed " + htmlFile.getAbsolutePath)
+  }
 
-	def processEtags(content: String, htmlFile: File): String = {
-		val crc32 = new CRC32()
+  def processEtags(content: String, htmlFile: File): String = {
+    val crc32 = new CRC32()
 
-		val regexp = """"([^"]+)\?crc=[^"]*"""".r
-		regexp.replaceAllIn(content, m => {
-			val path = m.group(1)
-			val dir = if (path.startsWith("/")) workingDir else htmlFile.getParent
-			val srcFile = new File(dir, path)
-			val srcIs = new FileInputStream(srcFile)
+    val regexp = """"([^"]+)\?crc=[^"]*"""".r
+    regexp.replaceAllIn(content, m => {
+      val path = m.group(1)
+      val dir = if (path.startsWith("/")) workingDir else htmlFile.getParent
+      val srcFile = new File(dir, path)
+      val srcIs = new FileInputStream(srcFile)
 
-			val bytes = try {
-				IOUtils.readFully(srcIs, srcFile.length().toInt, true)
-			} finally {
-				srcIs.close()
-			}
+      val bytes = try {
+        IOUtils.readFully(srcIs, srcFile.length().toInt, true)
+      } finally {
+        srcIs.close()
+      }
 
-			crc32.reset()
-			crc32.update(bytes)
-			val crc = crc32.getValue // In range [0, 2^32].
+      crc32.reset()
+      crc32.update(bytes)
+      val crc = crc32.getValue // In range [0, 2^32].
 
-			'"' + path + "?crc=" + crc.toHexString + '"'
-		})
-	}
+      '"' + path + "?crc=" + crc.toHexString + '"'
+    })
+  }
 }
